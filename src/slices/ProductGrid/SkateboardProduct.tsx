abb79bbe606d045a3f895e9c5bd4b7ac5ebd0126
Mboya72 <elvindio72@gmail.com>
2025-01-10 09:34:47 +0300

    adding the products name and border

--- src/slices/ProductGrid/SkateboardProduct.tsx
+++ src/slices/ProductGrid/SkateboardProduct.tsx
@@ -1,7 +1,9 @@
+import { ButtonLink } from '@/components/ButtonLink';
 import { createClient } from '@/prismicio';
 import { Content, isFilled } from '@prismicio/client';
 import { PrismicNextImage } from '@prismicio/next';
 import React from 'react'
+import { FaStar } from 'react-icons/fa6';
 
 type Props = {
     id: string;
@@ -17,11 +19,27 @@ export const SkateboardProduct = async ({ id }: Props) => {
         "Price Not Available";
 
     return (
-        <div>
+        <div className='group relative  mx-auto w-full max-w-72 px-8 pt-4 border-4'>
             <div className='flex items-center justify-between ~text-sm/2xl'>
                 <span>{price}</span>
+                <span className='inline-flex items-center gap-1'>
+                    <FaStar className='text-yellow-400' /> 37
+                </span>
             </div>
-            <PrismicNextImage alt="" field={product.data.image} width={150} />
+            <div className='-mb-1 overflow-hidden py-4'>
+                <PrismicNextImage alt="" field={product.data.image} width={150} 
+                className='mx-auto w-[58%]'/>
+            </div>
+
+            <h3 className='my-2 text-center font-sans leading-tight ~text-lg/xl'>
+                {product.data.name}
+            </h3>
+            <div className="absolute  inset-0 flex items-center justify-center opacity-0 transition-opacity
+                duration-200 group-hover:opacity-100">
+                <ButtonLink field={product.data.customizer}>
+                    Customize
+                </ButtonLink>
+            </div>
         </div>
     )
 }
